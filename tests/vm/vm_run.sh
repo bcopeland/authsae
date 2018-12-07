e59659677a94631bcf1f8071d8d5063aed0a3fb7
Bob Copeland <me@bobcopeland.com>
2018-12-07 11:13:22 -0500

    get kernel / rootfs from github

--- tests/vm/vm_run.sh
+++ tests/vm/vm_run.sh
@@ -4,20 +4,23 @@
 # kvm passthrough isn't available in circle ci
 QEMU=${QEMU:-qemu-system-x86_64}
 
+KERNEL=testing-vm-kernel
+ROOTFS=testing-vm-rootfs.img
+
 # download a VM kernel from some random location on the internet
 # the rootfs fstab mounts /dev/local with 9p in /local.  It should
 # have iw and so on installed.
-if [ ! -f authsae-vm-kernel ]; then
-    curl -O 'https://bobcopeland.com/srcs/authsae-vm-kernel'
+if [ ! -f $KERNEL ]; then
+    curl -O "https://github.com/bcopeland/testing-vm/releases/download/v1.0/$KERNEL"
 fi
-if [ ! -f authsae-vm.img ]; then
-    curl -O 'https://bobcopeland.com/srcs/authsae-vm.img.xz'
-    xz -d authsae-vm.img.xz
+if [ ! -f $ROOTFS ]; then
+    curl -O "https://github.com/bcopeland/testing-vm/releases/download/v1.0/$ROOTFS.xz"
+    xz -d $ROOTFS.xz
 fi
 
 $QEMU \
-  -kernel authsae-vm-kernel \
-  -drive file=authsae-vm.img,format=raw,if=virtio \
+  -kernel $KERNEL \
+  -drive file=$ROOTFS,format=raw,if=virtio \
   -fsdev local,security_model=none,id=fsdev-local,path=../.. \
   -device virtio-9p-pci,id=fs-local,fsdev=fsdev-local,mount_tag=/dev/local \
   -serial mon:stdio -nographic -vga none \
