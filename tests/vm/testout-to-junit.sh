15b51a30ef0500a37dee3b57f8243ee89a876fd9
Bob Copeland <me@bobcopeland.com>
2018-12-07 12:03:30 -0500

    include testsuites outer wrapper

--- tests/vm/testout-to-junit.sh
+++ tests/vm/testout-to-junit.sh
@@ -2,16 +2,18 @@
 # junit-ize the results from testout.log
 logfile=$1
 numtests=$(cat $logfile | grep -E "^test" | wc -l)
-echo '<testsuite tests="'$numtests'">'
+echo '<testsuites name="vmtests">'
+echo '  <testsuite tests="'$numtests'">'
 cat $logfile | grep -E "^test" | awk '{
    split($0, x, "PASS|FAIL: ", seps)
    testname=x[1];
    if (seps[1] == "PASS") {
-     print "  <testcase classname=\"authsae\" name=\"" testname "\"/>";
+     print "    <testcase classname=\"authsae\" name=\"" testname "\"/>";
    } else {
-     print "  <testcase classname=\"authsae\" name=\"" testname "\">";
-     print "    <failure type=\"fail\">" x[2] "</failure>";
-     print "  </testcase>";
+     print "    <testcase classname=\"authsae\" name=\"" testname "\">";
+     print "      <failure type=\"fail\">" x[2] "</failure>";
+     print "    </testcase>";
    }
 }'
-echo '</testsuite>'
+echo '  </testsuite>'
+echo '</testsuites>'
